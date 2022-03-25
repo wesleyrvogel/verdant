a25627effd940618a0023670678b776cd4c87f0c
Wesley Vogel <wesleyrvogel@gmail.com>
2022-03-25 15:48:52 -0700

    Added foam conductivity sensor and tested full system

--- python/system_control.py
+++ python/system_control.py
@@ -21,6 +21,16 @@ for pin in [37, 35, 33, 31]:
 valve_open = system_interface.GPIOController(29)
 valve_close = system_interface.GPIOController(32)
 
+def open_valve_for_time(s_open):
+    valve_open.set_state(True)
+    time.sleep(s_open)
+    valve_open.set_state(False)
+
+def close_valve_for_time(s_close):
+    valve_close.set_state(True)
+    time.sleep(s_close)
+    valve_close.set_state(False)
+
 def set_safe_state():
     # First, turn off all peristaltic pumps
     for pump in peristaltics:
@@ -42,6 +52,12 @@ co2_sensor = system_interface.CO2Sensor()
 adc = system_interface.ADCController()
 adc.set_full_scale_range(ADS1115Range.Range6_144V)
 
+def get_ph_reading():
+    return adc.get_channel_reading(0)
+
+def get_foam_conductivity_reading():
+    return adc.get_channel_reading(1)
+
 # This is a little state logger
 logger = system_interface.StateLogger('/home/pi/verdant_logs/')
 
@@ -50,13 +66,13 @@ set_safe_state()
 
 # Functional control loop
 errored = False  # Set to true to skip the loop during dev
+is_valve_open = True
 
 print('Starting control loop.')
 
 # Valve on!
-valve_open.set_state(True)
-time.sleep(.7)  # 700ms is approximately the minimum flow rate
-valve_open.set_state(False)
+# 700ms is approximately the minimum flow rate
+open_valve_for_time(.7)
 
 while not errored:
     try:
@@ -64,12 +80,14 @@ while not errored:
         # It takes 1s to read conductivity right now
         conductivity = conductivity_sensor.get_conductivity()
         co2 = co2_sensor.get_co2_concentration()
-        ph = adc.get_channel_reading(0)
+        ph = get_ph_reading()
+        foam_conductivity = get_foam_conductivity_reading()
         logger.log_values(co2, conductivity)
 
         # Determine what hardware needs to be actuated
         print('Conductivity: {} us/cm'.format(conductivity))
         print('pH Raw Reading: {} V'.format(ph))
+        print('Foam Proportional Wetness: {} V'.format(foam_conductivity))
         # Fresh water has conductivity around 10-30 us/cm
         # Pure salt water has closer to 15000 or more us/cm
         # Plants like around 800-1200 us/cm
@@ -80,6 +98,13 @@ while not errored:
             peristaltics[0].set_state(True)
             time.sleep(5)
             peristaltics[0].set_state(False)
+
+        if is_valve_open and foam_conductivity < 1.0:
+            close_valve_for_time(1)
+            is_valve_open = False
+        if not is_valve_open and foam_conductivity > 1.2:
+            open_valve_for_time(.7)
+            is_valve_open = True
 
         # Wait until next control period
         time.sleep(.1)  # Sleep for the loop here 
